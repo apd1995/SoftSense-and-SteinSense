b9a7f596eaaaaad2ffff80d4c21637892255dabd
Apratim Dey <85642502+apd1995@users.noreply.github.com>
2023-08-25 16:58:31 -0700

    Test block soft thresholding
    Main script is run_amp_blocksoft.py. Simplified a lot of the code by passing dicts as inputs and distinguishing between dicts of params and observables. Param dicts to be read from .json file.

--- amp_iteration.py
+++ amp_iteration.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 21 12:05:01 2023
+Created on Fri Aug 25 10:25:35 2023
 
 This script contains functions to compute a generic AMP iterate
 for matrix recovery given undersampled measurements.
-
-Right now this handles james stein. For other denoisers to write a more general
-script, one can use *args.
 
 The param 'noise_cov_.' always refers to noise covariance for a noisy signal.
 For example, noise_cov_current refers to noise covariance of the noisy signal (before applying denoiser)
@@ -17,8 +14,8 @@ at current iteration.
 """
 
 import numpy as np
-from james_stein_denoiser import james_stein_denoiser
-from denoiser_derivatives import james_stein_jacobian
+from block_soft_thresholding import block_soft_thresholding
+from denoiser_derivatives import block_soft_thresholding_jacobian
 
 
 def update_signal_noisy(A: float,
@@ -29,24 +26,27 @@ def update_signal_noisy(A: float,
 
 
 def update_signal_denoised(signal_noisy_current: float,
+                           tau: float,
                            noise_cov_current: float):
     
-    return np.apply_along_axis(james_stein_denoiser, 1, signal_noisy_current, noise_cov_current)
+    return np.apply_along_axis(block_soft_thresholding, 1, signal_noisy_current, tau, noise_cov_current)
 
 
 def onsager_matrix(signal_noisy_current: float,
-                       noise_cov_current: float,
-                       num_measurements: float):
+                   tau: float,
+                    noise_cov_current: float,
+                    num_measurements: float):
     
-    jacobian_array = np.apply_along_axis(james_stein_jacobian, 1, signal_noisy_current, noise_cov_current)
+    jacobian_array = np.apply_along_axis(block_soft_thresholding_jacobian, 1, signal_noisy_current, tau, noise_cov_current)
     return np.sum(jacobian_array, axis = 0)/num_measurements
 
 
 def onsager_term(Residual_prev: float,
                  signal_noisy_current: float,
+                 tau: float,
                  noise_cov_current: float):
 
-    onsager_matrix_ = onsager_matrix(signal_noisy_current, noise_cov_current, Residual_prev.shape[0])
+    onsager_matrix_ = onsager_matrix(signal_noisy_current, tau, noise_cov_current, Residual_prev.shape[0])
     return Residual_prev @ onsager_matrix_
 
 
@@ -55,10 +55,11 @@ def update_residual(A: float,
                     signal_noisy_current: float,
                     signal_denoised_current: float,
                     Residual_prev: float,
+                    tau: float,
                     noise_cov_current: float):
     
     naive_residual = Y - A @ signal_denoised_current
-    onsager_term_ = onsager_term(Residual_prev, signal_noisy_current, noise_cov_current)
+    onsager_term_ = onsager_term(Residual_prev, signal_noisy_current, tau, noise_cov_current)
     return naive_residual + onsager_term_
 
 
@@ -66,14 +67,17 @@ def amp_iteration(A: float,
                   Y: float,
                   signal_denoised_prev: float,
                   Residual_prev: float,
+                  tau: float,
                   noise_cov_current: float):
     
     signal_noisy_current = update_signal_noisy(A, signal_denoised_prev, Residual_prev)
     #noise_cov_current = Residual_prev.T @ Residual_prev/A.shape[0]
-    signal_denoised_current = update_signal_denoised(signal_noisy_current, noise_cov_current)
-    Residual_current = update_residual(A, Y, signal_noisy_current, signal_denoised_current, Residual_prev, noise_cov_current)
+    signal_denoised_current = update_signal_denoised(signal_noisy_current, tau, noise_cov_current)
+    Residual_current = update_residual(A, Y, signal_noisy_current, signal_denoised_current, Residual_prev, tau, noise_cov_current)
     return {'signal_denoised_current': signal_denoised_current,
             'Residual_current': Residual_current}
+
+
 
 
 
---
+++ minimax_tau_threshold.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 25 11:13:18 2023
+
+This script computes the minimax threshold for block soft thresholding. Assumes
+noise level is 1. notation follows Don-Joh-Mon (2013).
+
+Tested with R equivalent.
+
+@author: apratimdey
+"""
+
+from scipy import integrate
+from scipy.stats import chi2
+from scipy.optimize import fsolve
+import numpy as np
+
+
+def h_deno_integrand(x, tau, signal_ncol):
+    return  (np.sqrt(x) - tau) * chi2.pdf(x, df = signal_ncol)
+
+
+def h_deno_integral(tau, signal_ncol):
+    return integrate.quad(h_deno_integrand, tau**2, np.inf, args=(tau, signal_ncol))[0]
+
+
+def h(tau, signal_ncol):
+    numerator = tau
+    denominator = h_deno_integral(tau, signal_ncol)
+    return numerator/denominator
+
+
+def h_centered(tau, sparsity, signal_ncol):
+    return h(tau, signal_ncol) + 1 - (1/sparsity)
+
+
+def minimax_tau_threshold(sparsity, signal_ncol):
+    #root = newton(h_centered, 0, args=(sparsity, signal_ncol), maxiter = 1000, full_output=True)
+    root = fsolve(func = h_centered, x0 = -2, args = (sparsity, signal_ncol))
+    return root.item()
+
+minimax_tau_threshold(0.2, 5)
