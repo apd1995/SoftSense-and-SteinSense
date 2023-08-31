1fb695c6a8a19d26c248833c4e472fe4ab6befaa
adonoho <github@store.ddg.com>
2023-08-31 10:38:08 -0500

    Update to use a process per core with a single thread per process.

--- run_amp_blocksoft.py
+++ run_amp_blocksoft.py
@@ -232,14 +232,14 @@ def do_local_experiment():
 
 def read_and_do_local_experiment(json_file: str):
     exp = read_json(json_file)
-    with LocalCluster(dashboard_address='localhost:8787') as cluster:
+    with LocalCluster(dashboard_address='localhost:8787', n_workers=32, threads_per_worker=1) as cluster:
         with Client(cluster) as client:
             do_on_cluster(exp, run_amp_instance, client, credentials=get_gbq_credentials())
 
 
 def do_test_exp():
     exp = test_experiment()
-    with LocalCluster(dashboard_address='localhost:8787', n_workers=32, threads_per_worker=1) as cluster:
+    with LocalCluster(dashboard_address='localhost:8787') as cluster:
         with Client(cluster) as client:
             do_test_experiment(exp, run_amp_instance, client, credentials=get_gbq_credentials())
 
