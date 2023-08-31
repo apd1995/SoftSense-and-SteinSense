72340a563f45df00c7c772f1019fbb114e8e96da
Apratim Dey <85642502+apd1995@users.noreply.github.com>
2023-08-31 10:32:47 -0700

    Update run_amp_blocksoft.py

--- run_amp_blocksoft.py
+++ run_amp_blocksoft.py
@@ -260,7 +260,7 @@ def count_params(json_file: str):
 
 if __name__ == '__main__':
     # do_local_experiment()
-    read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_blocksoft_04.json')
+    read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_blocksoft_07.json')
     # count_params('updated_undersampling_int_grids.json')
     # do_coiled_experiment()
     # do_test_exp()
