00ef8cd56facb660e1e472f38f8e754be0d2f115
Apratim Dey <85642502+apd1995@users.noreply.github.com>
2023-09-28 13:18:42 -0700

    Corrected json filename

--- run_amp_normal_bayes.py
+++ run_amp_normal_bayes.py
@@ -502,9 +502,9 @@ def count_params(json_file: str):
 
 if __name__ == '__main__':
     # do_local_experiment()
-    # read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_normal_bayes_approx_jacobian.json')
+    # read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_normal_bayes_approx_jacobian_2.json')
     # count_params('updated_undersampling_int_grids.json')
-    do_coiled_experiment('exp_dicts/AMP_matrix_recovery_normal_bayes_approx_jacobian.json')
+    do_coiled_experiment('exp_dicts/AMP_matrix_recovery_normal_bayes_approx_jacobian_2.json')
     # do_test_exp()
     # do_test()
     # run_block_bp_experiment('block_bp_inputs.json')
