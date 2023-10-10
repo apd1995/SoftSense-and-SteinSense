2c5507a73f1321cddc73a12da4404090c7fceee5
Apratim Dey <85642502+apd1995@users.noreply.github.com>
2023-10-10 16:11:31 -0700

    Create run_amp_james_stein_regenerated.py
    Script to run AMP with james stein when the matrix is regenerated at every iteration.

---
+++ run_amp_james_stein_regenerated.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 11:22:03 2023
+
+@author: apratimdey
+"""
+
+import numpy as np
+from numpy.random import Generator
+import cvxpy as cvx
+from pandas import DataFrame
+import time
+# import amp_iteration as amp
+# from minimax_tau_threshold import minimax_tau_threshold
+
+from EMS.manager import do_on_cluster, get_gbq_credentials, do_test_experiment, read_json, unroll_experiment
+from dask.distributed import Client, LocalCluster
+import dask
+import coiled
+import logging
+import json
+
+logging.basicConfig(level=logging.INFO)
+log_gbq = logging.getLogger('pandas_gbq')
+log_gbq.setLevel(logging.DEBUG)
+log_gbq.addHandler(logging.StreamHandler())
+
+
+def seed(iter_count: int,
+         nonzero_rows: float,
+         num_measurements: float,
+         signal_nrow: float,
+         signal_ncol: float,
+         err_tol: float,
+         mc: float,
+         sparsity_tol: float) -> int:
+    return round(1 + round(iter_count*1000) + round(nonzero_rows * 1000) + round(num_measurements * 1000) + round(signal_nrow * 1000) + round(signal_ncol * 1000) + round(err_tol * 100000) + round(mc * 100000) + round(sparsity_tol * 1000000))
+
+
+def james_stein_nonsingular_vec(y, Sigma_inv):
+    d = len(y)
+    quad_whitening = np.dot(np.matmul(Sigma_inv, y), y)
+    if quad_whitening > 0:
+        return y * max(0, 1 - ((d-2)/quad_whitening))
+    else:
+        return y
+    
+
+def james_stein_nonsingular(X: np.ndarray, Sigma_inv: np.ndarray) -> np.ndarray:
+    """
+    Applies james stein rowwise to denoise Y.
+
+    Parameters
+    ----------
+    Y : np.ndarray
+        Noisy signal.
+    Sigma_inv : np.ndarray
+        Noise precision matrix.
+
+    Returns
+    -------
+    None.
+    """
+    d = X.shape[1]
+    quad_whitening = np.sum(X * np.matmul(X, Sigma_inv), axis=1)
+    james_stein_coeff = np.where(quad_whitening != 0, 1 - ((d-2)/quad_whitening), 1)
+    james_stein_coeff = np.where(james_stein_coeff > 0, james_stein_coeff, 0)
+    return X * james_stein_coeff[:, np.newaxis]
+
+
+def james_stein_diagonal_vec(y, diag_inv):
+    d = len(y)
+    quad_whitening = sum(diag_inv * y**2)
+    if quad_whitening > 0:
+        return y * max(0, 1 - ((d-2)/quad_whitening))
+    else:
+        return y
+    
+
+def james_stein_diagonal(X, diag_inv):
+    d = X.shape[1]
+    quad_whitening = np.sum(X**2 * diag_inv, axis=1)
+    james_stein_coeff = np.where(quad_whitening != 0, 1 - ((d-2)/quad_whitening), 1)
+    james_stein_coeff = np.where(james_stein_coeff > 0, james_stein_coeff, 0)
+    return X * james_stein_coeff[:, np.newaxis]
+
+
+def james_stein_singular_vec(y, Sigma_eigvecs, nonzero_indices, Sigma_nonzero_eigvals_inv):
+    # changing coordinates to get uncorrelated components
+    y_indep = np.matmul(Sigma_eigvecs.T, y)
+    
+    y_indep_nonzero =  y_indep[nonzero_indices]
+    
+    # apply denoiser on these coordinates to estimate the signal in the new basis on indep coordinates
+    signal_newbasis_indep = james_stein_diagonal_vec(y_indep_nonzero, Sigma_nonzero_eigvals_inv)
+    
+    # when D has a 0 entry, it means we have perfect precision
+    zero_indices = ~nonzero_indices
+    y_indep_zero = y_indep[zero_indices]
+    signal_newbasis_zero = y_indep_zero
+    
+    # combine the two to get signal_newbasis
+    signal_newbasis = np.concatenate((signal_newbasis_zero, signal_newbasis_indep))
+    # signal_newbasis = np.zeros(len(y), dtype = float)
+    # signal_newbasis[nonzero_indices] = signal_newbasis_indep
+    # signal_newbasis[zero_indices] = signal_newbasis_zero
+    
+    # we have identified U.T @ signal, now we need to get signal i.e revert to original coordinates
+    signal_originalbasis = np.matmul(Sigma_eigvecs, signal_newbasis)
+    
+    return signal_originalbasis
+
+
+def james_stein_singular(X, Sigma_eigvecs, nonzero_indices, Sigma_nonzero_eigvals_inv):
+    # changing coordinates to get uncorrelated components
+    X_indep = np.matmul(X, Sigma_eigvecs)
+    
+    X_indep_nonzero =  X_indep[:, nonzero_indices]
+    
+    # apply denoiser on these coordinates to estimate the signal in the new basis on indep coordinates
+    signal_newbasis_indep = james_stein_diagonal(X_indep_nonzero, Sigma_nonzero_eigvals_inv)
+    
+    # when D has a 0 entry, it means we have perfect precision
+    zero_indices = ~nonzero_indices
+    X_indep_zero = X_indep[:, zero_indices]
+    signal_newbasis_zero = X_indep_zero
+    
+    # combine the two to get signal_newbasis
+    signal_newbasis = np.zeros(X.shape, dtype = float)
+    signal_newbasis[:, nonzero_indices] = signal_newbasis_indep
+    signal_newbasis[:, zero_indices] = signal_newbasis_zero
+    
+    # we have identified U.T @ signal, now we need to get signal i.e revert to original coordinates
+    signal_originalbasis = np.matmul(signal_newbasis, Sigma_eigvecs.T)
+    
+    return signal_originalbasis
+
+
+def update_signal_noisy(A: float,
+                        signal_denoised_prev: float,
+                        Residual_prev: float):
+    return signal_denoised_prev + np.matmul(A.T, Residual_prev)
+
+
+def update_signal_denoised_nonsingular(signal_noisy_current: float,
+                                       noise_cov_current_inv: float):
+    return james_stein_nonsingular(signal_noisy_current, noise_cov_current_inv)
+
+
+def update_signal_denoised_singular(signal_noisy_current: float,
+                                    noise_cov_current_eigvecs: float,
+                                    noise_cov_current_nonzero_indices,
+                                    noise_cov_current_nonzero_eigvals_inv: float):
+    return james_stein_singular(signal_noisy_current, noise_cov_current_eigvecs,
+                                            noise_cov_current_nonzero_indices, noise_cov_current_nonzero_eigvals_inv)
+
+
+def update_residual(A: float,
+                             Y: float,
+                             signal_denoised_current: float):
+    naive_residual = Y - np.matmul(A, signal_denoised_current)
+    return naive_residual
+
+
+def gen_iid_normal_mtx(num_measurements, signal_nrow, rng):
+    """
+    Generates a single random num_measurements by signal_nrow matrix with iid signal_nrow(0,1) entries
+
+    Parameters
+    ----------
+    num_measurements : int
+        Number of rows of measurement matrix.
+    signal_nrow : int
+        Number of rows of signal matrix.
+
+    Returns
+    -------
+    numpy.ndarray
+        num_measurements by signal_nrow matrix.
+
+    """
+    return rng.normal(0, 1, (num_measurements, signal_nrow))
+
+
+def recovery_stats(X_true: float,
+              X_rec: float,
+              sparsity_tol: float):
+    
+    N, B = X_true.shape
+
+    zero_indices_true = (np.apply_along_axis(np.linalg.norm, 1, X_true)==0)
+    zero_indices_rec = (np.apply_along_axis(np.linalg.norm, 1, X_rec)/np.sqrt(B)<=10*sparsity_tol)
+
+    nonzero_indices_true = (np.apply_along_axis(np.linalg.norm, 1, X_true)!=0)
+    nonzero_indices_rec = (np.apply_along_axis(np.linalg.norm, 1, X_rec)/np.sqrt(B)>10*sparsity_tol)
+    
+    dict_observables = {
+                'rel_err': cvx.norm(X_true-X_rec, "fro").value/cvx.norm(X_true, "fro").value,
+                'avg_err': cvx.norm(X_true - X_rec, "fro").value/np.sqrt(N*B),
+                'max_row_err': cvx.mixed_norm(X_true - X_rec, 2, np.inf).value/np.sqrt(B),
+                'norm_2_1_true': cvx.mixed_norm(X_true, 2, 1).value/(N*np.sqrt(B)),
+                'norm_2_1_rec': cvx.mixed_norm(X_rec, 2, 1).value/(N*np.sqrt(B)),
+                'norm_2_2_true': cvx.mixed_norm(X_true, 2, 2).value/np.sqrt(N*B),
+                'norm_2_2_rec': cvx.mixed_norm(X_rec, 2, 2).value/np.sqrt(N*B),
+                'norm_2_infty_true': cvx.mixed_norm(X_true, 2, np.inf).value/np.sqrt(B),
+                'norm_2_infty_rec': cvx.mixed_norm(X_rec, 2, np.inf).value/np.sqrt(B),
+                'soft_sparsity': np.mean(np.apply_along_axis(np.linalg.norm, 1, X_rec)/np.sqrt(B) > 10*sparsity_tol),
+                'nonzero_rows_rec': np.sum(np.apply_along_axis(np.linalg.norm, 1, X_rec)/np.sqrt(B) > 10*sparsity_tol),
+                'tpr': sum(zero_indices_true * zero_indices_rec)/max(1, sum(zero_indices_true)),
+                'tnr': sum(nonzero_indices_true * nonzero_indices_rec)/max(1, sum(nonzero_indices_true))
+                }
+    
+    return dict_observables
+
+
+def run_amp_instance(**dict_params):
+    
+    k = dict_params['nonzero_rows']
+    n = dict_params['num_measurements']
+    N = dict_params['signal_nrow']
+    B = dict_params['signal_ncol']
+    err_tol = dict_params['err_tol']
+    mc = dict_params['mc']
+    sparsity_tol = dict_params['sparsity_tol']
+    max_iter = dict_params['max_iter']
+    err_explosion_tol = dict_params['err_explosion_tol']
+    selected_rows_frac = dict_params['selected_rows_frac']
+
+    iter_count = 0
+    
+    rng = np.random.default_rng(seed=seed(iter_count, k, n, N, B, err_tol, mc, sparsity_tol))
+
+    signal_true = np.zeros((N, B), dtype=float)
+    nonzero_indices = rng.choice(range(N), k, replace=False)
+    signal_true[nonzero_indices, :] = rng.normal(0, 1, (k, B))
+   
+    A = gen_iid_normal_mtx(n, N, rng)/np.sqrt(n)
+    Y = np.matmul(A, signal_true)
+    
+    sparsity = k/N
+    dict_params['sparsity'] = sparsity
+    dict_params['undersampling_ratio'] = n/N
+    
+    tick = time.perf_counter()
+    
+    signal_denoised_current = np.zeros((N, B), dtype = float)
+    Residual_current = Y
+    
+    dict_observables = recovery_stats(signal_true,
+                               signal_denoised_current,
+                               sparsity_tol)
+    rel_err = dict_observables['rel_err']
+    min_rel_err = rel_err
+    # noise_cov_current = np.matmul(Residual_current.T, Residual_current)/n
+    noise_cov_current_cov = np.cov(Residual_current.T)
+    
+    while iter_count<max_iter and rel_err>100*err_tol and rel_err<err_explosion_tol:
+        
+        iter_count = iter_count + 1
+
+        rng = np.random.default_rng(seed=seed(iter_count, k, n, N, B, err_tol, mc, sparsity_tol))
+        A_prev = A
+        A_current = gen_iid_normal_mtx(n, N, rng)/np.sqrt(n)
+        Y_current = np.matmul(A_current, signal_true)
+        
+        signal_denoised_prev = signal_denoised_current
+        signal_denoised_current = None
+        Residual_prev = Residual_current
+        Residual_current = None
+        noise_cov_current_cov = np.cov(Residual_prev.T)
+        
+        D, U = np.linalg.eigh(noise_cov_current_cov)
+        D = np.round(D, 10)
+        
+        if np.all(D > 0):
+            noise_cov_current_inv = np.matmul(U * 1.0/D, U.T)
+            signal_noisy_current = update_signal_noisy(A_prev, signal_denoised_prev, Residual_prev)
+            signal_denoised_current = update_signal_denoised_nonsingular(signal_noisy_current, noise_cov_current_inv)
+            Residual_current = update_residual(A_current,
+                                            Y_current,
+                                            signal_denoised_current)
+        else:
+            nonzero_indices = (D > 0)
+            D_nonzero_inv = 1/D[nonzero_indices]
+            signal_noisy_current = update_signal_noisy(A_prev, signal_denoised_prev, Residual_prev)
+            signal_denoised_current = update_signal_denoised_singular(signal_noisy_current, U,
+                                                                      nonzero_indices,
+                                                                      D_nonzero_inv)
+            Residual_current = update_residual(A_current,
+                                            Y_current,
+                                            signal_denoised_current)
+        
+        dict_observables = recovery_stats(signal_true,
+                                   signal_denoised_current,
+                                   sparsity_tol)
+        rel_err = dict_observables['rel_err']
+        min_rel_err = min(rel_err, min_rel_err)
+    
+    tock = time.perf_counter() - tick
+    dict_observables['min_rel_err'] = min_rel_err
+    dict_observables['iter_count'] = iter_count
+    dict_observables['time_seconds'] = round(tock, 2)
+
+    #return DataFrame(data = {**dict_params, **dict_observables}).set_index('iter_count')
+    return DataFrame(data = {**dict_params, **dict_observables}, index = [0])
+
+
+def test_experiment() -> dict:
+    exp = {'nonzero_rows': 30,
+           'num_measurements': 50,
+           'signal_nrow': 100,
+           'signal_ncol': 5,
+           'max_iter': 50,
+           'err_tol': 1e-5,
+           'sparsity_tol': 1e-4,
+           'err_explosion_tol': 1e+5,
+           'mc': 0}
+    # exp = dict(table_name='amp-integer-grids',
+    #             base_index=0,
+    #             db_url='sqlite:///data/EMS.db3',
+    #             multi_res=[{
+    #                 'nonzero_rows': list(range(1, 100)),
+    #                 'num_measurements': list(range(1, 100)),
+    #                 'signal_nrow': [100],
+    #                 'signal_ncol': [1, 2, 3, 4, 5],
+    #                 'mc': list(range(100)),
+    #                 'err_tol': [1e-5],
+    #                 'sparsity_tol': [1e-4]
+    #             }])
+    # exp = dict(table_name='comr-N50-larger-grids',
+    #             base_index=0,
+    #             db_url='sqlite:///data/EMS.db3',
+    #             multi_res=[{
+    #                 'nonzero_rows': list(range(1, 50)),
+    #                 'num_measurements': list(range(1, 50)),
+    #                 'signal_nrow': [50],
+    #                 'signal_ncol': [1, 2, 3, 4, 5],
+    #                 'mc': list(range(100)),
+    #                 'err_tol': [1e-5],
+    #                 'sparsity_tol': [1e-4]
+    #             }])
+    return exp
+
+
+def do_coiled_experiment(json_file: str):
+    exp = read_json(json_file)
+    logging.info(f'{json.dumps(dask.config.config, indent=4)}')
+    software_environment = 'adonoho/amp_matrix_recovery'
+    # coiled.delete_software_environment(name=software_environment)
+    logging.info('Creating environment.')
+    coiled.create_software_environment(
+        name=software_environment,
+        conda="environment-coiled.yml",
+        pip=[
+            "git+https://GIT_TOKEN@github.com/adonoho/EMS.git"
+        ]
+    )
+    with coiled.Cluster(software=software_environment,
+                        n_workers=960, worker_vm_types=['n1-standard-1'],
+                        use_best_zone=True, spot_policy='spot') as cluster:
+        with Client(cluster) as client:
+            do_on_cluster(exp, run_amp_instance, client, credentials=get_gbq_credentials())
+
+
+def do_local_experiment():
+    exp = test_experiment()
+    logging.info(f'{json.dumps(dask.config.config, indent=4)}')
+    with LocalCluster(dashboard_address='localhost:8787') as cluster:
+        with Client(cluster) as client:
+            do_on_cluster(exp, run_amp_instance, client, credentials=get_gbq_credentials())
+
+
+def read_and_do_local_experiment(json_file: str):
+    exp = read_json(json_file)
+    with LocalCluster(dashboard_address='localhost:8787', n_workers=32, threads_per_worker=1) as cluster:
+    # with LocalCluster(dashboard_address='localhost:8787') as cluster:
+        with Client(cluster) as client:
+            # do_on_cluster(exp, run_amp_instance, client, credentials=None)
+            do_on_cluster(exp, run_amp_instance, client, credentials=get_gbq_credentials())
+
+
+def do_test_exp():
+    exp = test_experiment()
+    with LocalCluster(dashboard_address='localhost:8787') as cluster:
+        with Client(cluster) as client:
+            do_test_experiment(exp, run_amp_instance, client, credentials=get_gbq_credentials())
+
+
+def do_test():
+    exp = test_experiment()
+    print(exp)
+    pass
+    df = run_amp_instance(dict_params = exp)
+    df.to_csv("temp.csv")
+
+
+def count_params(json_file: str):
+    exp = read_json(json_file)
+    params = unroll_experiment(exp)
+    logging.info(f'Count of instances: {len(params)}.')
+
+
+if __name__ == '__main__':
+    # do_local_experiment()
+    read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_JS_normal_cov_regenerated.json')
+    # count_params('updated_undersampling_int_grids.json')
+    # do_coiled_experiment('exp_dicts/AMP_matrix_recovery_JS_approx_jacobian_normal_cov_random_undersampling.json')
+    # do_test_exp()
+    # do_test()
+    # run_block_bp_experiment('block_bp_inputs.json')
