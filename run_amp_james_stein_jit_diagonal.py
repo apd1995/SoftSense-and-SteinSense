341ec7044f71f6eb43469a6ef289f30367ad33db
Apratim Dey <85642502+apd1995@users.noreply.github.com>
2024-05-03 09:17:14 -0700

    Update run_amp_james_stein_jit_diagonal.py

--- run_amp_james_stein_jit_diagonal.py
+++ run_amp_james_stein_jit_diagonal.py
@@ -537,7 +537,7 @@ def do_local_experiment():
 
 def read_and_do_local_experiment(json_file: str):
     exp = read_json(json_file)
-    with LocalCluster(dashboard_address='localhost:8787', n_workers=4) as cluster:
+    with LocalCluster(dashboard_address='localhost:8787', n_workers=32) as cluster:
         with Client(cluster) as client:
             # do_on_cluster(exp, run_amp_instance, client, credentials=None)
             do_on_cluster(exp, run_amp_instance, client, credentials=get_gbq_credentials())
@@ -566,10 +566,10 @@ def count_params(json_file: str):
 
 if __name__ == '__main__':
     # do_local_experiment()
-    # read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_JS_poisson_jit.json')
+    read_and_do_local_experiment('exp_dicts/AMP_matrix_recovery_JS_normal_jit_diagonal.json')
     # count_params('updated_undersampling_int_grids.json')
     # do_coiled_experiment('exp_dicts/AMP_matrix_recovery_JS_normal_jit_daveUPenn.json')
-    do_sherlock_experiment('exp_dicts/AMP_matrix_recovery_JS_normal_jit_diagonal.json')
+    # do_sherlock_experiment('exp_dicts/AMP_matrix_recovery_JS_normal_jit_diagonal.json')
     # do_test_exp()
     # do_test()
     # run_block_bp_experiment('block_bp_inputs.json')
